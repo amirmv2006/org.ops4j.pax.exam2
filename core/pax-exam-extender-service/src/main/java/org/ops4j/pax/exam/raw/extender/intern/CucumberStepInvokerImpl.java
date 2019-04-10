c88f0ffa58b4e890818a5e919bd038cfa3aa60c7
Amir <AmirMV2006@gmail.com>
2019-04-10 17:58:24 +0200

    fixup

--- core/pax-exam-extender-service/src/main/java/org/ops4j/pax/exam/raw/extender/intern/CucumberStepInvokerImpl.java
+++ core/pax-exam-extender-service/src/main/java/org/ops4j/pax/exam/raw/extender/intern/CucumberStepInvokerImpl.java
@@ -1,6 +1,8 @@
 package org.ops4j.pax.exam.raw.extender.intern;
 
 import org.ops4j.pax.exam.ICucumberStepInvoker;
+import org.ops4j.pax.exam.util.Injector;
+import org.ops4j.pax.swissbox.tracker.ServiceLookup;
 import org.osgi.framework.BundleContext;
 
 import java.lang.reflect.Method;
@@ -8,9 +10,11 @@ import java.lang.reflect.Method;
 public class CucumberStepInvokerImpl
         implements ICucumberStepInvoker {
     private BundleContext ctx;
+    private Injector injector;
 
     public CucumberStepInvokerImpl(String expr, BundleContext ctx) {
         this.ctx = ctx;
+        injector = ServiceLookup.getService(ctx, Injector.class);
     }
 
     @Override
@@ -23,7 +27,9 @@ public class CucumberStepInvokerImpl
         for (Method declaredMethod : declaredMethods) {
             if (declaredMethod.getName().equals(classAndMethodName.substring(lastIndexOf + 1))) {
                 // keep instances of steps
-                return (R) declaredMethod.invoke(stepClass.newInstance(), args);
+                Object newInstance = stepClass.newInstance();
+                injector.injectFields(newInstance);
+                return (R) declaredMethod.invoke(newInstance, args);
             }
         }
         throw new RuntimeException("Can not find address " + caption);
